90ff35ba64feeb9ad5fefbb24336044e9b92fcc7
Oliver Giczi <oleeeyminecraft@gmail.com>
2024-01-18 22:21:09 +0100

    Figuren-Klasse und Bauern-Subklasse erstellt, Zeichnungsfunktion

--- chess.py
+++ chess.py
@@ -5,19 +5,39 @@ GAME_WIDTH = 600
 GAME_HEIGHT = 600
 BLOCK_SIZE = GAME_WIDTH / 8
 
-class Figure:
-    def __init__(self, t, c):
-        self.tile = t
-        self.color = c
-
-    class Pawn:
-        pass
-
 win = Tk()
 win.title("Schachbrett")
 win.resizable(False, False)
 
-pawnImg = PhotoImage(file="img\pawnW.png")
+class Figure():
+    def __init__(self):
+       pass
+
+    def drawImg(self, t, c):
+        img = PhotoImage(file="img\\{0}-pawn.png".format(c))
+        win.img = img
+        canvas.create_image(canvas.coords(t)[0], canvas.coords(t)[1], image=img, anchor=NW)
+        
+
+    def getTile(self):
+        return self.tile
+    
+    def getColor(self):
+        return self.color
+    
+
+
+class Pawn(Figure):
+    def __init__(self, t, c):
+        self.tile = t
+        self.color = c
+
+        pawnImg = PhotoImage(file="img\white-pawn.png")
+        canvas.create_image(canvas.coords("a1")[0], canvas.coords("a1")[1], image=pawnImg, anchor=NW)
+
+        super().drawImg(self.tile, self.color)
+    
+
 
 canvas = Canvas(win, width=GAME_WIDTH, height=GAME_HEIGHT, background="gray", bd=-2)
 canvas.pack()
@@ -41,8 +61,6 @@ for num in range(1, 9):
         x1 = BLOCK_SIZE * (chars.index(char) + 1)
         y1 = GAME_HEIGHT - (BLOCK_SIZE	* (num - 1))
         canvas.create_rectangle(x0, y0, x1, y1, fill=color, width=0, tag=char + str(num))
-    
-canvas.create_image(canvas.coords("a1")[0], canvas.coords("a1")[1], image=pawnImg, anchor=NW)
 
 win.update()
 
@@ -54,5 +72,11 @@ x = int((screenWidth/2) - (winWidth/2))
 y = int((screenHeight/2) - (winHeight/2))
 
 win.geometry(f"{winWidth}x{winHeight}+{x}+{y}")
+
+
+pawnW = Pawn("a2", "white")
+
+
+
 
 win.mainloop()
