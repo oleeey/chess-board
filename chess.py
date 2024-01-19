e03a421a64ebe103e3cede40f3d26bf156983d87
Oliver Giczi <oleeeyminecraft@gmail.com>
2024-01-19 19:55:02 +0100

    Update chess.py

--- chess.py
+++ chess.py
@@ -9,41 +9,37 @@ win = Tk()
 win.title("Schachbrett")
 win.resizable(False, False)
 
+figureImages = {
+                "white": {
+                    "pawn": PhotoImage(file="img\\white-pawn.png"),
+                    "bishop": PhotoImage(file="img\\white-bishop.png"),
+                    "rook": PhotoImage(file="img\\white-rook.png"),
+                    "knight": PhotoImage(file="img\\white-knight.png"),
+                    "king": PhotoImage(file="img\\white-king.png"),
+                    "queen": PhotoImage(file="img\\white-queen.png")}, 
+                "black": {
+                    "pawn": PhotoImage(file="img\\black-pawn.png"),
+                    "bishop": PhotoImage(file="img\\black-bishop.png"),
+                    "rook": PhotoImage(file="img\\black-rook.png"),
+                    "knight": PhotoImage(file="img\\black-knight.png"),
+                    "king": PhotoImage(file="img\\black-king.png"),
+                    "queen": PhotoImage(file="img\\black-queen.png")}
+                }
+
 class Figure():
-    def __init__(self):
-       pass
-
-    def drawImg(self, t, c, img):      
-        canvas.create_image(canvas.coords(t)[0], canvas.coords(t)[1], image=img, anchor=NW)
+    def __init__(self, t, t2, c):
+        self.type = t
+        self.tile = t2
+        self.color = c
+        self.img = figureImages[self.color][self.type]
+        canvas.create_image(canvas.coords(self.tile)[0], canvas.coords(self.tile)[1], image=self.img, anchor=NW)
         
-
     def getTile(self):
         return self.tile
     
     def getColor(self):
         return self.color
     
-
-class Pawn(Figure):
-    global whitePawn, blackPawn
-    whitePawn = PhotoImage(file="img\\white-pawn.png")
-    blackPawn = PhotoImage(file="img\\black-pawn.png")
-
-    
-    def __init__(self, t, c):
-        self.tile = t
-        self.color = c
-        self.image = ""
-
-        if self.color == "white":
-            self.image = whitePawn
-        else:
-            self.image = blackPawn
-
-        super().drawImg(self.tile, self.color, self.image)
-
-
-        
     
 canvas = Canvas(win, width=GAME_WIDTH, height=GAME_HEIGHT, background="gray", bd=-2)
 canvas.pack()
@@ -81,15 +77,28 @@ win.geometry(f"{winWidth}x{winHeight}+{x}+{y}")
 
 def drawFigures():
     for i in range(0,8):
-        tile = chars[i] + str(2)
-        print(tile)
-        Pawn(tile, "white")
+        Figure("pawn", chars[i] + str(2), "white")
+        Figure("pawn", chars[i] + str(7), "black")
 
-    for i in range(0,8):
-        tile = chars[i] + str(7)
-        print(tile)
-        Pawn(tile, "black")
+    for c in ["a", "h"]:       
+        Figure("rook", c + str(1), "white")
+        Figure("rook", c + str(8), "black")
 
+    for c in ["b", "g"]:
+        Figure("knight", c + str(1), "white")
+        Figure("knight", c + str(8), "black")
+
+    for c in ["c", "f"]:
+        Figure("bishop", c + str(1), "white")
+        Figure("bishop", c + str(8), "black")
+
+    Figure("queen", "d1", "white")
+    Figure("queen", "d8", "black")
+
+    Figure("king", "e1", "white")
+    Figure("king", "e8", "black")
+    
 drawFigures()
+        
 
 win.mainloop()
